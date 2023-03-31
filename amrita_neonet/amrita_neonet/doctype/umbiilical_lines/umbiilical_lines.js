fef88ebef1f0bcef94464339a810311ba9e25a7d
purush34 <purush3190@gmail.com>
2023-03-31 16:21:40 +0530

    2nd backup

---
+++ amrita_neonet/amrita_neonet/doctype/umbiilical_lines/umbiilical_lines.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, ICTS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Umbiilical lines', {
+	// refresh: function(frm) {
+
+	// }
+});
